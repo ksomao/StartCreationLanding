d187b9e357e498504286c7ac2b282f9a6195a5c3
ksomao <somao@outlook.com>
2020-11-11 15:28:18 +0100

    withTheme added

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import styled from "styled-components";
+import styled, {withTheme} from "styled-components";
 import Header from "./parts/Header";
 import Space from "./parts/Space";
 import Events from "./parts/Events";
@@ -22,25 +22,7 @@ const IndexPage = () => {
     )
 }
 
-export default IndexPage
+export default withTheme(IndexPage)
 
-const Span = styled.span`
-font-weight: bold;
-color: orange;
-  &:nth-child(1){
-    margin-left: 50px;
-    font-size: 2em;
-    display: block;
-  }
-  &:nth-child(2){
-    font-size: 2em;
-    display: block;
-    margin-left: 20px;
-  }
-  &:nth-child(3){
-    margin-left: 50px;
-    font-size: 2em;
-    display: block;
-  }
-`
+
 
